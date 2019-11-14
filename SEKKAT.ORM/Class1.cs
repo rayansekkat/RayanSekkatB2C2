a965bc40be7af71b8a24882ecf3dc33afe4938f1
Rayan SEKKAT <rayan.sekkat@epsi.fr>
2019-11-14 13:20:38 +0100

    test

---
+++ SEKKAT.ORM/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEKKAT.ORM
+{
+    public class Class1
+    {
+        /*
+        public virtual Categorie Categories { get; set; } // one to many
+
+        public virtual ICollection<Proprietaire> Proprietaires { get; set; } // many to many 
+
+        public Chaton()
+        {
+            Proprietaires = new List<Proprietaire>();
+        }*/
+    }
+}
---
+++ SEKKAT.ORM/ContextBDD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatonsCodeFirst.ORM
+{
+    public class ContexteBDD : DbContext
+    {
+        public ContexteBDD()
+            : base("chaineDeConnexion")
+        {
+
+        }
+
+        //public DbSet<Categorie> Categories { get; set; }
+        //public DbSet<Chaton> Chatons { get; set; }
+        //public DbSet<Proprietaire> Proprietaires { get; set; }
+    }
+}
+
